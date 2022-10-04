9ce01f4258c51b3ce026729409f25862f5ab0eb0
unknown <aniruddhvaish2024@gmai.com>
2022-10-04 16:41:25 +0530

    Added

--- app.js
+++ app.js
@@ -433,3 +433,17 @@ window.onclick = function(event) {
 
 var day = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"}) 
 document.getElementById("d2").innerHTML = day;
+
+
+var icon=document.getElementById("icon");
+icon.onclick=function(){
+console.log("hello")
+document.body.classList.toggle("dark-theme");
+if(document.body.classList.contains("dark-theme"))
+{
+icon.src="images/dark theme icon/sun.png";
+}
+else{
+icon.src="images/dark theme icon/moon.png" 
+} 
+}
