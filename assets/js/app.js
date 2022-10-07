5d7b606692773a19e959e3f9d7f7a995a6ea56de
Datalog-Mindhack <vandanajha92283@gmail.com>
2022-10-07 22:51:36 +0530

    added dark mode feature

--- assets/js/app.js
+++ assets/js/app.js
@@ -466,6 +466,14 @@ window.onclick = function (event) {
   }
 }
 
-
+var icon = document.getElementById("icon");
+icon.onclick = function(){
+  document.body.classList.toggle("dark-theme");
+  if(document.body.classList.contains("dark-theme")){
+    icon.src = "images/sun.png";
+  }else{
+    icon.src = "images/moon.png";
+  }
+}
 
 
